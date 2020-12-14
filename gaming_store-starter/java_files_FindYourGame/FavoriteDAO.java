c2ef7c09571129601f9c69d1f8dff4c41ad6df9f
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2020-12-14 20:30:04 +0200

    finished Favorite and FavoriteDAO classes

--- gaming_store-starter/java_files_FindYourGame/FavoriteDAO.java
+++ gaming_store-starter/java_files_FindYourGame/FavoriteDAO.java
@@ -9,32 +9,16 @@ public class FavoriteDAO {
         DB db = new DB();
         Connection con = null;
         PreparedStatement stmt = null;
-        String addFavoriteSql = "INSER INTO favorite (game_id, user_id) VALUES (?, ?); UPDATE game SET favorite_counter = favorite_counter + 1 WHERE game_id = ?";   
+        String addFavoriteSql = "INSERT INTO favorite (game_id, registered_user_id) VALUES (?, ?); UPDATE game SET favorite_counter = favorite_counter + 1 WHERE game_id = ?;";   
         // it creates a new row in favorite and adds one to the favorite counter of the favorited game
         try {
             
             con = db.getConnection();
 
-            stmt = con.prepareStatement(addFavoritSql);
+            stmt = con.prepareStatement(addFavoriteSql);
             stmt.setInt(1, game_id);
-            stmt.setString(2, student.getEmail());
-            ResultSet rs = stmt.executeQuery();
- 
-            if (rs.next()) {
-                rs.close();
-                stmt.close();
-                throw new Exception("AM or Email already exists");
-            }
-
-            rs.close();
-            
-            stmt = con.prepareStatement(sql);
-
-            stmt.setString(1, student.getAm());
-            stmt.setString(2, student.getName());
-            stmt.setString(3, student.getSurname());
-            stmt.setString(4, student.getEmail());
-            stmt.setInt(5, student.getDepartmentId());
+            stmt.setInt(2, user_id);
+            stmt.setInt(3, game_id);
 
             stmt.executeUpdate();
             
@@ -53,4 +37,81 @@ public class FavoriteDAO {
         }
 
     }
+
+    public void removesFromFavorite(int user_id, int game_id) throws Exception {
+
+
+        DB db = new DB();
+        Connection con = null;
+        PreparedStatement stmt = null;
+        String addFavoriteSql = "DELETE FROM favorite WHERE game_id = ? AND registered_user_id = ?; UPDATE game SET favorite_counter = favorite_counter - 1 WHERE game_id = ?;";   
+        // it removes row in favorite and removes one from the favorite counter of the favorited game
+        try {
+            
+            con = db.getConnection();
+
+            stmt = con.prepareStatement(addFavoriteSql);
+            stmt.setInt(1, game_id);
+            stmt.setInt(2, user_id);
+            stmt.setInt(3, game_id);
+
+            stmt.executeUpdate();
+            
+            stmt.close();
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+
+        }
+
+    }
+
+    public boolean isGameFavorite(int game_id, int user_id) throws Exception {
+
+		DB db = new DB();
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sqlQuery = "SELECT * FROM favorite WHERE game_id = ? AND registered_user_id = ?;";
+
+		try {
+
+			con = db.getConnection();
+			stmt = con.prepareStatement(sqlQuery);
+            stmt.setInt(1, game_id);
+            stmt.setInt(2, user_id);
+
+			rs = stmt.executeQuery();
+
+			if (!rs.next()) {
+				return false;
+			}
+
+			rs.close();
+			stmt.close();
+			db.close();
+
+			return true;
+
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+
+	}
 }
