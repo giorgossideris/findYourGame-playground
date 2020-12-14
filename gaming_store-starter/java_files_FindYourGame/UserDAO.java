245e948347ab5cd750f8b3339a69d14917b866a8
kwnpsychogiou <konnapsy@gmail.com>
2020-12-14 21:22:25 +0200

    added searchUserByID

--- gaming_store-starter/java_files_FindYourGame/UserDAO.java
+++ gaming_store-starter/java_files_FindYourGame/UserDAO.java
@@ -99,4 +99,49 @@ public class UserDAO {
             
         }
     }
+    
+    public User searchUserByID(int user_id) throws Exception {
+
+		DB db = new DB();
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sqlQuery = "SELECT * FROM registered_user WHERE registered_user_id=?;";
+
+		try {
+
+			con = db.getConnection();
+			stmt = con.prepareStatement(sqlQuery);
+			stmt.setInt(1, user_id);
+
+			rs = stmt.executeQuery();
+
+			if (!rs.next()) {
+				throw new Exception("User with id: " + user_id + " not found");
+			}
+
+			User user = new User(rs.getInt("registered_user_id"), rs.getString("username"), rs.getString("password"), rs.getString("email"));
+			rs.close();
+			stmt.close();
+			db.close();
+
+			return user;
+
+
+		} catch (Exception e) {
+
+            throw new Exception(e.getMessage());
+            
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+
+	} 
+
 }
