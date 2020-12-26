dd39b4c2f4e8749bf27491e294e67118d3bf2f28
giorgossideris <56915448+giorgossideris@users.noreply.github.com>
2020-12-26 21:49:43 +0200

    made sorting.js redirect to home page

--- gaming_store-starter/js/sorting.js
+++ gaming_store-starter/js/sorting.js
@@ -4,5 +4,5 @@ for (let i=0; i<sort_types.length; i++){
 }
 
 function use_sort_type(){
-    $.post("./sortingController.jsp", { eidos_sort: this.dataset.sortValue});
+    window.location = './sortingController.jsp?eidos_sort='+this.dataset.sortValue;
 }
